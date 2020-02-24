4d723fdeb6edb1d7226ef58061d8a12da384ca56
Kamal Mahmassani <mahmassani_k@subway.com>
2020-02-24 19:34:45 +0200

    adding logger

--- PaymentGateway/PaymentGateway.API/Program.cs
+++ PaymentGateway/PaymentGateway.API/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace PaymentGateway.API
@@ -17,8 +11,14 @@ namespace PaymentGateway.API
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+            })
+            .UseStartup<Startup>();
     }
 }
